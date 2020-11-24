a2e75051a68eafdfc56b7680a0ff0312e29637c6
timebe00 <an0160@naver.com>
2020-11-24 18:09:04 +0900

    add menupage

--- CafeJava/src/main/java/com/example/demo/repository/MenuRepository.java
+++ CafeJava/src/main/java/com/example/demo/repository/MenuRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     List<Menu> findByPlace(String place);
+    List<Menu> findByMenuNo(Long menuNo);
 }
